f95efc0400572e34f15f369769fcd75ee10ba2a0
Rizky <riskyasyam@gmail.com>
2024-05-19 20:19:31 +0700

    Update History Pemesanan, Update Freebank Soal
    History Pemesanan Update :
    - Button Lanjutkan route ke detail paketku
    - Button Detail Order route ke detail pemesanan
    - Button Sertifikat route ke halaman download sertifikat
    - Free Bank Soal update tampilan

--- lib/card/Histori Pemesanan/selesaiorders_card.dart
+++ lib/card/Histori Pemesanan/selesaiorders_card.dart
@@ -1,4 +1,8 @@
+import 'package:bumn_muda/screens/Histori%20Pemesanan/detailorders_screen.dart';
+import 'package:bumn_muda/screens/Histori%20Pemesanan/sertifikat_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class SelesaiOrdersCard extends StatelessWidget {
   final String judul;
@@ -77,40 +81,57 @@ class SelesaiOrdersCard extends StatelessWidget {
                   SizedBox(height: 10,),
                   Row(
                     children: [
-                      Container(
-                        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 7),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Color(0xff2E3D64),
-                        ),
-                        child: Text(
-                          'Sertifikat',
-                          style: TextStyle(
-                            fontFamily: 'Poppins',
-                            fontSize: 10,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white
+                      GestureDetector(
+                        onTap: (){
+                          Navigator.push(
+                            context, 
+                            MaterialPageRoute(
+                              builder: (context) => SertifikatScreen(judulPaket: judul,)));
+                        },
+                        child: Container(
+                          padding: EdgeInsets.symmetric(horizontal: 12, vertical: 7),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Color(0xff2E3D64),
+                          ),
+                          child: Text(
+                            'Sertifikat',
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 10,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white
+                            ),
                           ),
                         ),
                       ),
                       SizedBox(width: 5,),
-                      Container(
-                        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.white,
-                          border: Border.all(
-                            width: 2,
-                            color: Color(0xff2E3D64)
-                          )
-                        ),
-                        child: Text(
-                          'Detail Order',
-                          style: TextStyle(
-                            fontFamily: 'Poppins',
-                            fontSize: 10,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xff2E3D64)
+                      GestureDetector(
+                        onTap: (){
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      DetailOrders(judulPaket: judul)));
+                        },
+                        child: Container(
+                          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white,
+                            border: Border.all(
+                              width: 2,
+                              color: Color(0xff2E3D64)
+                            )
+                          ),
+                          child: Text(
+                            'Detail Order',
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 10,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xff2E3D64)
+                            ),
                           ),
                         ),
                       ),
--- lib/screens/Paketku/freebank_screen.dart
+++ lib/screens/Paketku/freebank_screen.dart
@@ -1,3 +1,5 @@
+import 'package:bumn_muda/card/unduhpdf.dart';
+import 'package:bumn_muda/card/unduhpdf_card.dart';
 import 'package:flutter/material.dart';
 
 class FreeBankScreen extends StatefulWidget {
@@ -10,6 +12,87 @@ class FreeBankScreen extends StatefulWidget {
 class _FreeBankScreenState extends State<FreeBankScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        title: RichText(
+          text: const TextSpan(
+            style: TextStyle(
+              fontFamily: 'Poppins',
+              fontSize: 17,
+              fontWeight: FontWeight.bold,
+              color: Color(0xff2E3D64),
+            ),
+            children: [
+              TextSpan(
+                text: 'Free Bank ',
+                style: TextStyle(
+                  // Gaya teks untuk "Video"
+                  fontWeight: FontWeight.w500,
+                  // Atur gaya teks sesuai keinginan Anda
+                ),
+              ),
+              TextSpan(
+                text: 'Soal',
+                // Gaya teks untuk "Pembelajaran"
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Paket BUMN 1',
+              style: TextStyle(
+                fontFamily: 'Poppins',
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Color(0xff2E3D64)
+              ),
+            ),
+            SizedBox(height: 10,),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+              decoration: BoxDecoration(
+                color: Color(0xffE7F0FD),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Text(
+                'PDF',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 14,
+                  fontWeight: FontWeight.w600,
+                  color: Color(0xff2E3D64),
+                ),
+              ),
+            ),
+            Container(
+              child: ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: products.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: UnduhPDFCard(
+                      namaFile: products[index].namaFile,
+                      halaman: products[index].halaman,
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
